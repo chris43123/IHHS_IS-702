d599ee609cb82ad0392c0ee85c51b8635ddc1958
chris43123 <46337972+chris43123@users.noreply.github.com>
2021-08-07 22:37:41 -0600

    Setup DB connection

--- centromedico/config/db.php
+++ centromedico/config/db.php
@@ -1,13 +1,12 @@
 <?php
-
 //Clase y metodo de conexion a la db
 class Database{
     public static function connect(){
         $db = new mysqli(
-            'localhost',
-            'root',
-            '',
-            'centromedico'
+            '192.241.138.101',
+            'sa',
+            'PumasUNAH.2021',
+            'IHSS_DevOps'
         );
         //Set parra que las consultas devuelvan caracteres del español
         $db->query("SET NAMES 'utf8'");
